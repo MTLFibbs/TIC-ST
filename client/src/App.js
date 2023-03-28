501aaa47abfa1660bb50553c81df686aae832f53
MTLFibbs <119969967+MTLFibbs@users.noreply.github.com>
2023-03-28 11:43:57 -0400

    created server and client

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+
+const App = () => {
+  return (
+    <main>
+      POG
+    </main>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const express = require('express');
+const morgan = require('morgan');
+
+const PORT = 8888;
+
+
+express()
+.use(function(req, res, next) {
+    res.header(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, HEAD, GET, PUT, POST, DELETE'
+    );
+    res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
+})
+.use(morgan('tiny'))
+.use(express.static('./server/assets'))
+.use(express.json())
+.use(express.urlencoded({ extended: false }))
+.use('/', express.static(__dirname + '/'))
+
+// REST endpoints
+
+.get("/test",(req,res) => {
+    res.status(200).json({status: 200, message: "this worked?"})
+})
+
+// this is our catch all endpoint.
+.get("*", (req, res) => {
+    res.status(404).json({
+    status: 404,
+    message: "This is obviously not what you are looking for.",
+    });
+})
+
+.listen(PORT, () => console.info(`Listening on port ${PORT}`));
